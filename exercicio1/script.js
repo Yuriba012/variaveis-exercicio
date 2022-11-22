85441ed6e16114bfb25f48d7852a5404d4334167
unknown <yuriba012@gmail.com>
2022-11-21 23:04:16 -0300

    Exercicios 1, 2 e 3 resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,30 @@
+//ETAPA 1
+// let nome
+// let idade
+
+// console.log(typeof(nome))
+// console.log(typeof(idade))
+
+// // Os tipos ainda são undefined
+// // não foram atribuídos valores
+// // para essas variáveis
+
+// nome = prompt("Digite seu nome: ")
+// idade = Number(prompt("Digite sua idade: "))
+
+// console.log(typeof(nome))
+// console.log(typeof(idade))
+
+// console.log("Seu nome é",nome,"e você tem",idade,"anos")
+
+let msg1 = "Você mora no Brasil?"
+let msg2 = "De qual estado você é?"
+let msg3 = "De qual cidade você é?"
+
+let resposta1 = prompt(msg1)
+let resposta2 = prompt(msg2)
+let resposta3 = prompt(msg3)
+
+console.log(msg1,"Resposta:",resposta1)
+console.log(msg2,"Resposta:",resposta2)
+console.log(msg3,"Resposta:",resposta3)
---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+let a = prompt("Digite alguma coisa")
+let b = prompt("Digite alguma coisa novamente")
+
+console.log(a,b)
+
+let c = a
+a = b
+b = c
+
+console.log(a,b)
